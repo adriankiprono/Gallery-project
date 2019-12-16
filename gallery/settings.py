2f647fa858e7f7c12195009b1fae7dfc94b1e7bf
adriankiprono <tuimuradrian6@gmail.com>
2019-12-16 10:00:17 +0300

    add link and navigation

--- gallery/settings.py
+++ gallery/settings.py
@@ -31,14 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'image.apps.ImageConfig',
+    'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'image.apps.ImageConfig',
-    'bootstrap4',
+    
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
--- image/views.py
+++ image/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.http  import HttpResponse
+from django.shortcuts import render,redirect
+from django.http  import HttpResponse,Http404
 import datetime as dt
 
 
@@ -23,6 +23,16 @@ def news_of_day(request):
         </html>
             '''
     return HttpResponse(html)
+def convert_dates(dates):
+    
+    # Function that gets the weekday number for the date.
+    day_number = dt.date.weekday(dates)
+
+    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
+
+    # Returning the actual day of the week
+    day = days[day_number]
+    return day
 def news_of_day(request):
     date = dt.date.today()
 
@@ -54,3 +64,23 @@ def past_days_news(request,past_date):
         </html>
             '''
     return HttpResponse(html)
+def news_of_day(request):
+    date = dt.date.today()
+    return render(request, 'all-image/today-news.html', {"date": date,})
+
+
+def past_days_news(request, past_date):
+    
+    try:
+        # Converts data from the string Url
+        date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
+
+    except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()
+        assert False
+
+    if date == dt.date.today():
+        return redirect(news_of_day)
+
+    return render(request, 'all-image/past-news.html', {"date": date})
