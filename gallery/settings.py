e1253cd73c6123ebbaf071c294a7eed8e3db0a33
adriankiprono <tuimuradrian6@gmail.com>
2019-12-16 08:54:07 +0300

    add static

--- gallery/settings.py
+++ gallery/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'image.apps.ImageConfig',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
