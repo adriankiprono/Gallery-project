f6c5181722599f726f9e0fd16c63d7597fbbd20a
adriankiprono <tuimuradrian6@gmail.com>
2019-12-17 16:19:37 +0300

    add js in base

--- static/js/base_images.js
+++ static/js/base_images.js
@@ -1,8 +1,12 @@
 $(function(){
-    $("#copy").click(function(){
+    $("#copypaste").click(function(){
         $(this).focus();
         $(this).select();
-        document.execCommand("copy");
+        document.execCommand('copypaste');
         $(this).after('copy to clipboard');
     });
 });
+copyLink =(element) => {
+    document.getElementById(element).select();
+    document.execCommand("copy");
+  };
